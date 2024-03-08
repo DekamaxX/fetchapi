261c192d1a5cb867ea9147ca285d074972d1c7cb
DekamaxX <dobosz.mateusz1994@gmail.com>
2024-03-08 22:12:57 +0100

    change function

--- js/fetch.js
+++ js/fetch.js
@@ -16,17 +16,20 @@ const searchParams = {
 return axios.get(`${MovieApi.BASE_URL}trending/movie/week`, searchParams);
 
 // zapytanie o gatunki
-fetchTrendingMovies();
-const searchParams = {
-  params: {
-    page: this.page,
-    api_key: MovieApi.API_KEY,
-  },
-};
-return axios.get(`${MovieApi.BASE_URL}genre/movie/list`, searchParams);
+export async function getGenres() {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`
+    );
+    const respGenres = await response.json();
 
+    return respGenres;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 // zapytanie po id
-fetchTrendingId();
+fetchMovieId();
 const searchParams = {
   params: {
     page: this.page,
@@ -35,4 +38,6 @@ const searchParams = {
 };
 return axios.get(`${MovieApi.BASE_URL}id/movie/list`, searchParams);
 
-// zapytanie query
+// query
+
+// export
