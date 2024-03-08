56a2dfef868b79f1600f4ae42c56cc452cd5fada
DekamaxX <dobosz.mateusz1994@gmail.com>
2024-03-08 21:40:11 +0100

    fist fetch

--- js/fetch.js
+++ js/fetch.js
@@ -1,2 +1,15 @@
 "use strict";
+// axios
+import axios from 'axios'
 // projekt fetch api movie
+const API_KEY = "";
+const BASE_URL = "";
+
+fetchTrendingMovies(){
+    const searcParams={
+        params:{
+            page: this.page,
+            api_key: movieApi.API_KEY,
+        }
+    }
+};
