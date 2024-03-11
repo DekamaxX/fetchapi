06a2db775a03a023510489aca4ff265e0050a567
DekamaxX <dobosz.mateusz1994@gmail.com>
2024-03-11 20:51:28 +0100

    final js

--- js/fetch.js
+++ js/fetch.js
@@ -6,15 +6,19 @@ const API_KEY = "";
 const BASE_URL = "";
 
 // zapytanie o popularne filmy
-fetchTrendingMovies();
-const searchParams = {
-  params: {
-    page: this.page,
-    api_key: MovieApi.API_KEY,
-  },
-};
-return axios.get(`${MovieApi.BASE_URL}trending/movie/week`, searchParams);
+const getTrendingMovies = async (page = 1) => {
+  try {
+    const resp = await fetch(
+      `${BASE_URL}/trending/all/day?api_key=${API_KEY}&language=en-US&page=${page}`
+    );
 
+    if (!resp.ok) throw new Error(resp.status);
+
+    return await resp.json();
+  } catch (err) {
+    console.error(err.message);
+  }
+};
 // zapytanie o gatunki
 export async function getGenres() {
   try {
@@ -29,15 +33,30 @@ export async function getGenres() {
   }
 }
 // zapytanie po id
-fetchMovieId();
-const searchParams = {
-  params: {
-    page: this.page,
-    api_key: MovieApi.API_KEY,
-  },
-};
-return axios.get(`${MovieApi.BASE_URL}id/movie/list`, searchParams);
+export async function fetchTrailer(id) {
+  try {
+    const responce = await fetch(
+      `${FilmsApi.BASE_URL}/movie/${id}/videos?api_key=${FilmsApi.API_KEY}`
+    );
+    const data = await responce.json();
+
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
 // query
+export async function fetchFilmsByQuery() {
+  const searchParams = {
+    params: {
+      page: this.page,
+      api_key: FilmsApi.API_KEY,
+      query: this.query,
+    },
+  };
+
+  return axios.get(`${FilmsApi.BASE_URL}search/movie`, searchParams);
+}
 
 // export
